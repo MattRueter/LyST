592df636c4c322db7b526e30b401a8fcf61b6679
Robert Matthew Rueter <84277551+MattRueter@users.noreply.github.com>
2023-09-05 14:07:10 -0500

    Delete oldsrc directory

--- src/data/todos.js
+++
@@ -1,6 +0,0 @@
-const todos =[
-    {todo: "shopping", when:"Friday"},
-    {todo: "study French", when:"Wednesday"},
-    {todo: "learn Redux", when: "Tuesday"},
-]
-export { todos };
--- src/data/users.js
+++
@@ -1,6 +0,0 @@
-const users =[
-    {uid: 555, username:"matt", role:"admin"},
-    {uid:777, username: "vic", role:"user"}
-]
-
-export {users};
